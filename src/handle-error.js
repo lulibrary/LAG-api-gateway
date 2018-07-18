7f72a2d1558ed102fe175175cfd4ace673c39884
Dom Belcher <dominic.belcher@gmail.com>
2018-07-18 15:56:05 +0100

    added error handler to replace unsafe errors with safe http error

---
+++ src/handle-error.js
@@ -0,0 +1,16 @@
+const HTTPError = require('node-http-error')
+
+const handleError = (error) => {
+  if (!(error instanceof HTTPError)) {
+    error = new HTTPError(500, 'Internal Server Error')
+  }
+
+  return {
+    statusCode: error.status,
+    body: JSON.stringify({
+      message: error.message
+    })
+  }
+}
+
+module.exports = handleError
---
+++ test/handle-error-test.js
@@ -0,0 +1,44 @@
+// Test libraries
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinonChai = require('sinon-chai')
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+chai.should()
+
+const sinon = require('sinon')
+const sandbox = sinon.createSandbox()
+
+// Module dependencies
+const HTTPError = require('node-http-error')
+
+// Module under test
+const handleError = require('../src/handle-error')
+
+describe('user path handler', () => {
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should return the passed error if it is an http error', () => {
+    const testError = new HTTPError(404, 'File not found!')
+
+    handleError(testError).should.deep.equal({
+      statusCode: 404,
+      body: JSON.stringify({
+        message: 'File not found!'
+      })
+    })
+  })
+
+  it('should replace the error with a 500 internal server error if it is not an HTTP error', () => {
+    const testError = new Error('something went wrong')
+
+    handleError(testError).should.deep.equal({
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal Server Error'
+      })
+    })
+  })
+})
