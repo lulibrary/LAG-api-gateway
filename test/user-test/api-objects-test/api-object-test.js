84bb8a22b48366031f4b9095da40aaf164a0707f
Dom Belcher <dominic.belcher@gmail.com>
2018-07-25 11:08:58 +0100

    added tests for ApiObject class

---
+++ test/user-test/api-objects-test/api-object-test.js
@@ -0,0 +1,69 @@
+// Test libraries
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinonChai = require('sinon-chai')
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+chai.should()
+
+const sinon = require('sinon')
+const sandbox = sinon.createSandbox()
+
+const uuid = require('uuid/v4')
+
+const rewire = require('rewire')
+let wires = []
+
+const AlmaClient = require('alma-api-wrapper')
+
+// Module under test
+const ApiObject = rewire('../../../src/user/api-objects/api-object')
+
+describe('base api object tests', () => {
+  afterEach(() => {
+    sandbox.restore()
+    wires.forEach(wire => wire())
+    wires = []
+  })
+
+  describe('_ensureApi method tests', () => {
+    it('should resolve with the alma Api if it already exists', () => {
+      const testObject = new ApiObject()
+      const testApi = {
+        api: uuid()
+      }
+
+      testObject.almaApi = testApi
+
+      return testObject._ensureApi().should.eventually.deep.equal(testApi)
+    })
+
+    it('should call getAlmaApiKey if the alma Api doesn\'t exist', () => {
+      const getKeyStub = sandbox.stub()
+      getKeyStub.resolves()
+      wires.push(ApiObject.__set__('getAlmaApiKey', getKeyStub))
+
+      process.env.ALMA_KEY = 'key'
+      const testObject = new ApiObject()
+
+      return testObject._ensureApi()
+        .then(() => {
+          getKeyStub.should.have.been.calledOnce
+        })
+    })
+
+    it('should create a new instance of AlmaClient on the Object', () => {
+      const getKeyStub = sandbox.stub()
+      getKeyStub.resolves()
+      wires.push(ApiObject.__set__('getAlmaApiKey', getKeyStub))
+
+      process.env.ALMA_KEY = 'key'
+      const testObject = new ApiObject()
+
+      return testObject._ensureApi()
+        .then(() => {
+          testObject.almaApi.should.be.an.instanceOf(AlmaClient)
+        })
+    })
+  })
+})
