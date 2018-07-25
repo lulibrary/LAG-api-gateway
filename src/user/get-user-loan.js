9f253c53abc5845fbd0d69da20e1161857457ae5
Dom Belcher <dominic.belcher@gmail.com>
2018-07-25 11:13:53 +0100

    removed redundant code from get-user and get-user-loan

--- src/user/get-user-loan.js
+++ src/user/get-user-loan.js
@@ -1,32 +1,6 @@
 'use strict'
-
-const Schemas = require('@lulibrary/lag-alma-utils')
-const { Queue } = require('@lulibrary/lag-utils')
-const AlmaClient = require('alma-api-wrapper')
-
-const _pick = require('lodash.pick')
 const handleError = require('../handle-error')
-const apiError = require('../api-error')
-
 const ApiUserLoan = require('./api-objects/api-user-loan')
-
-const LoanModel = Schemas.LoanSchema(process.env.LOAN_CACHE_TABLE_NAME)
-
-const loanApiFields = [
-  'loan_id',
-  'user_id',
-  'renewable',
-  'call_number',
-  'loan_status',
-  'due_date',
-  'item_barcode',
-  'mms_id',
-  'title',
-  'author',
-  'description',
-  'publication_year',
-  'process_status'
-]
 
 module.exports.handle = (event, context, callback) => {
   const userID = event.pathParameters.userID
@@ -44,27 +18,4 @@ module.exports.handle = (event, context, callback) => {
       console.log(e)
       callback(handleError(e))
     })
-}
-
-const handleUserLoan = (userID, loanID) => {
-  return LoanModel.get(loanID)
-    .then(user => user
-      ? formatCacheLoan(user)
-      : (sendToQueue(loanID), Promise.reject())
-    )
-    .catch(e => getUserFromApi(userID, loanID))
-}
-
-const sendToQueue = (message) => {
-  return new Queue({ url: process.env.LOANS_QUEUE_URL })
-    .sendMessage(message)
-}
-
-const formatCacheLoan = loan => _pick(loan, loanApiFields)
-
-const getUserFromApi = (userID, loanID) => {
-  return new AlmaClient().users.for(userID).getLoan(loanID)
-    .catch(apiError)
-    .then(loan => loan.data)
-    .then(loanData => _pick(loanData, loanApiFields))
 }
--- src/user/get-user.js
+++ src/user/get-user.js
@@ -1,22 +1,6 @@
 'use strict'
-const Schemas = require('@lulibrary/lag-alma-utils')
-const { Queue } = require('@lulibrary/lag-utils')
-const AlmaClient = require('alma-api-wrapper')
-
-const _pick = require('lodash.pick')
 const handleError = require('../handle-error')
-const apiError = require('../api-error')
-const getAlmaApiKey = require('../get-alma-api-key')
-
-const UserModel = Schemas.UserSchema(process.env.USER_CACHE_TABLE_NAME)
-
 const ApiUser = require('./api-objects/api-user')
-
-const userApiFields = [
-  'primary_id',
-  'loans',
-  'requests'
-]
 
 module.exports.handle = (event, context, callback) => {
   const userID = event.pathParameters.userID
@@ -33,35 +17,3 @@ module.exports.handle = (event, context, callback) => {
       callback(handleError(e))
     })
 }
-
-/*
-const handleUser = (userID) => {
-  return UserModel.get(userID)
-    .then(user => user
-      ? formatCacheUser(user)
-      : (sendToQueue(userID), Promise.reject())
-    )
-    .catch(e => getUserFromApi(userID))
-}
-
-const sendToQueue = (message) => {
-  return new Queue({ url: process.env.USERS_QUEUE_URL })
-    .sendMessage(message)
-}
-
-const formatCacheUser = user => {
-  return {
-    primary_id: user.primary_id,
-    loans: user.loan_ids,
-    requests: user.request_ids
-  }
-}
-
-const getUserFromApi = userID => {
-  return getAlmaApiKey()
-    .then(() => new AlmaClient().users.get(userID))
-    .catch(apiError)
-    .then(user => user.data)
-    .then(userData => _pick(userData, userApiFields))
-}
-*/
