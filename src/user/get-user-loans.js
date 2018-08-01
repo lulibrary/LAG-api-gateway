335223c502e030f86cc0d563037e657be71c4691
Dom Belcher <dominic.belcher@gmail.com>
2018-08-01 12:10:10 +0100

    removed redundant code

--- src/user/get-user-loans.js
+++ src/user/get-user-loans.js
@@ -1,15 +1,7 @@
 'use strict'
 
-const { Queue } = require('@lulibrary/lag-utils')
-
 const handleError = require('../handle-error')
-
-// const LoanModel = Schemas.LoanSchema(process.env.LOAN_CACHE_TABLE_NAME)
-
-// const resolveUserLoan = require('./loans/resolve-user-loan')
-
 const ApiUser = require('./api-objects/api-user')
-const ApiUserLoan = require('./api-objects/api-user-loan')
 
 module.exports.handle = (event, context, callback) => {
   const userID = event.pathParameters.userID
