09e7e6b888f2cf2e35d4ebd55552a6f8be03db43
Dom Belcher <dominic.belcher@gmail.com>
2018-08-09 15:09:57 +0100

    added get-user-request handler

---
+++ src/user/get-user-request.js
@@ -0,0 +1,21 @@
+'use strict'
+const handleError = require('../handle-error')
+const ApiUserRequest = require('./api-objects/api-user-request')
+
+module.exports.handle = (event, context, callback) => {
+  const userID = event.pathParameters.userID
+  const requestID = event.pathParameters.requestID
+
+  // handleUserRequest(userID, requestID)
+  new ApiUserRequest().get(userID, requestID)
+    .then(response => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      })
+    })
+    .catch(e => {
+      console.log(e)
+      callback(handleError(e))
+    })
+}
---
+++ test/user-test/get-user-request-test.js
@@ -0,0 +1,223 @@
+// Test libraries
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinonChai = require('sinon-chai')
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+chai.should()
+
+const sinon = require('sinon')
+const sandbox = sinon.createSandbox()
+
+const nock = require('nock')
+
+const uuid = require('uuid/v4')
+
+const AWS_MOCK = require('aws-sdk-mock')
+let mocks = []
+
+process.env.ALMA_API_KEY_NAME = 'key'
+const testTableName = `test_request_table_${uuid()}`
+process.env.REQUEST_CACHE_TABLE_NAME = testTableName
+const testQueueUrl = `test_requests_queue_${uuid()}`
+process.env.REQUESTS_QUEUE_URL = testQueueUrl
+
+let stubs = require('../mocks.js')
+const getItemStub = stubs.getItemStub
+const describeTableStub = stubs.describeTableStub
+
+// Module under test
+let userPathHandler = require('../../src/user/get-user-request')
+const handle = (event, ctx) => new Promise((resolve, reject) => {
+  userPathHandler.handle(event, ctx, (err, res) => {
+    return err ? reject(err) : resolve(res)
+  })
+})
+
+const mockTable = (tableName) => {
+  describeTableStub.callsArgWith(1, null, {
+    Table: {
+      AttributeDefinitions: [{
+        AttributeName: 'request_id',
+        KeyType: 'HASH'
+      }],
+      KeySchema: [{
+        AttributeName: 'request_id',
+        KeyType: 'HASH'
+      }],
+      ItemCount: 100,
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+      },
+      TableArn: `arn:aws:dynamodb:table:someuser:${tableName}`,
+      TableName: tableName,
+      TableStatus: 'ACTIVE'
+    }
+  })
+}
+
+describe('request path end to end tests', function () {
+  this.timeout(10000)
+
+  before(() => {
+    mockTable(testTableName)
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+    getItemStub.reset()
+    mocks.forEach(mock => AWS_MOCK.restore(mock))
+    mocks = []
+  })
+
+  it('should query the Cache for a Request record', () => {
+    process.env.ALMA_KEY = 'key'
+
+    const testUserID = `test_user_${uuid()}`
+    const testRequestID = `test_request_${uuid()}`
+    const testUserRecord = {
+      Item: {
+        request_id: {
+          S: testRequestID
+        },
+        expiry_date: {
+          N: '1600000000'
+        }
+      }
+    }
+    getItemStub.callsArgWith(1, null, testUserRecord)
+    // AWS_MOCK.mock('DynamoDB', 'getItem', getItemStub)
+    const getParameterStub = sandbox.stub()
+    getParameterStub.callsArgWith(1, null, { Parameter: { Value: 'key' } })
+    AWS_MOCK.mock('SSM', 'getParameter', getParameterStub)
+    mocks.push('SSM')
+
+    return handle({
+      pathParameters: {
+        userID: testUserID,
+        requestID: testRequestID
+      }
+    }, {})
+      .then(res => {
+        getItemStub.should.have.been.calledWith({
+          TableName: testTableName,
+          Key: {
+            request_id: {
+              S: testRequestID
+            }
+          }
+        })
+      })
+  })
+
+  it('should query the Alma API if no Request is in the Cache', () => {
+    AWS_MOCK.mock('SQS', 'sendMessage', {})
+    getItemStub.callsArgWith(1, null, { })
+    // AWS_MOCK.mock('DynamoDB', 'getItem', cacheGetStub)
+    mocks.push('SQS')
+    const getParameterStub = sandbox.stub()
+    getParameterStub.callsArgWith(1, null, { Parameter: { Value: 'key' } })
+    AWS_MOCK.mock('SSM', 'getParameter', getParameterStub)
+    mocks.push('SSM')
+
+    const testUserID = `test_user_${uuid()}`
+    const testRequestID = `test_request_${uuid()}`
+
+    let urlQueries = []
+
+    const alma = nock('https://api-eu.hosted.exlibrisgroup.com')
+      .get((uri) => {
+        urlQueries.push(uri)
+        return true
+      })
+      .reply(200, {
+        request_id: testRequestID
+      })
+
+    return handle({
+      pathParameters: {
+        userID: testUserID,
+        requestID: testRequestID
+      }
+    }, {})
+      .then(() => {
+        urlQueries.should.include(`/almaws/v1/users/${testUserID}/requests/${testRequestID}?format=json`)
+      })
+  })
+
+  it('should call SQS#sendMessage if no Request is in the Cache', () => {
+    const sendMessageStub = sandbox.stub()
+    sendMessageStub.callsArgWith(1, null, true)
+    AWS_MOCK.mock('SQS', 'sendMessage', sendMessageStub)
+    getItemStub.callsArgWith(1, null, { })
+    // AWS_MOCK.mock('DynamoDB', 'getItem', cacheGetStub)
+    mocks.push('SQS')
+    const getParameterStub = sandbox.stub()
+    getParameterStub.callsArgWith(1, null, { Parameter: { Value: 'key' } })
+    AWS_MOCK.mock('SSM', 'getParameter', getParameterStub)
+    mocks.push('SSM')
+
+    const testUserID = `test_user_${uuid()}`
+    const testRequestID = `test_request_${uuid()}`
+
+    nock('https://api-eu.hosted.exlibrisgroup.com')
+      .get(uri => true)
+      .reply(200, {
+        request_id: testRequestID
+      })
+
+    return handle({
+      pathParameters: {
+        userID: testUserID,
+        requestID: testRequestID
+      }
+    })
+      .then(() => {
+        sendMessageStub.should.have.been.calledWith({
+          QueueUrl: testQueueUrl,
+          MessageBody: JSON.stringify({
+            userID: testUserID,
+            requestID: testRequestID
+          })
+        })
+      })
+  })
+
+  it('should return an error if it cannot get the request from the cache or the API', () => {
+    AWS_MOCK.mock('SQS', 'sendMessage', {})
+    getItemStub.callsArgWith(1, null, { })
+    // AWS_MOCK.mock('DynamoDB', 'getItem', cacheGetStub)
+    mocks.push('SQS')
+    const getParameterStub = sandbox.stub()
+    getParameterStub.callsArgWith(1, null, { Parameter: { Value: 'key' } })
+    AWS_MOCK.mock('SSM', 'getParameter', getParameterStub)
+    mocks.push('SSM')
+
+    // sandbox.stub(console, 'log')
+
+    const testUserID = `test_user_${uuid()}`
+    const testRequestID = `test_request_${uuid()}`
+
+    let urlQueries = []
+
+    const alma = nock('https://api-eu.hosted.exlibrisgroup.com')
+      .get((uri) => {
+        urlQueries.push(uri)
+        return true
+      })
+      .reply(400, {
+        message: 'Missing API key'
+      })
+
+    return handle({
+      pathParameters: {
+        userID: testUserID,
+        requestID: testRequestID
+      }
+    }, {})
+      .catch(e => {
+        e.statusCode.should.equal(400)
+      })
+  })
+})
